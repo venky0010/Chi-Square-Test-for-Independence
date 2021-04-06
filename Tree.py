601cffa9ea533b6a68a75e83cf76ad1801e98158
Venkatesh Mohan Sharma <63727056+venky0010@users.noreply.github.com>
2021-04-06 08:30:57 +0530

    Update Tree.py
    Included Graphviz library

--- Tree.py
+++ Tree.py
@@ -1,6 +1,6 @@
+
 import csv
 from collections import defaultdict
-import pydotplus
 import scipy.stats
 import pandas as pd
 import re
@@ -9,12 +9,12 @@ class DecisionTree:
     def __init__(self, branches=[]):
        # self.col = col
         self.branches=branches
-
+gr=[]
 def growDecisionTreeFrom(dataset,cnsqnt,ante,df_with_antecedent):
     #dataset dataframe does not contain antecedent columns
     if len(dataset) == 0: return DecisionTree()
     child=p_value(ante, cnsqnt, dataset,df_with_antecedent)#function to find the column in raw file with lowest p value.Fuction should return a tuple with child_name and p_value
-    #print(ante)
+    gr.append(ante)
     branch=[]
     print(ante)
     for x in child:
@@ -36,8 +36,8 @@ def growDecisionTreeFrom(dataset,cnsqnt,ante,df_with_antecedent):
 
 def find_columns(df):
     
-    raw = pd.read_csv('GRIT10Raw.csv')
-    
+    raw = pd.read_csv('0204Raw.csv')
+    raw = raw.drop(['SOURCE_APPN_IDS_MI'], axis=1)
     total_columns = {}
     for string in raw.columns[1:]:
         for column in df.columns:
@@ -77,7 +77,7 @@ def calculate_p_value(ante, cons, column, df,df_with_ante): #ante and cons are c
         k+=1
     for i in range(len(tp_row)):
         if tp_row[i] == 0 and fn_row[i] == 0:
-          return [0, 1, 0]
+            return [0, 1, 0]
 
     if len(tp_row) == 1:
         return [0, 1, 0]
@@ -112,19 +112,80 @@ def p_value(antecedent, consequent, df,df_with_ante):
     return child_nodes
 
 
-cnsqnt='not_SECONDBIT=1_APP_ORGIN_RCG'
-ante=['not_PEC/QTZ_SOURCE_APPLICATION_ID_MI']
-data1= pd.read_csv('GRIT23Bin.csv')
+cnsqnt='SECONDBIT=0_APP_ORGIN_RCG'
+ante=['NOT_PEC_AND_QTZ_SOURCE_APPN_IDS_MI']
+data1= pd.read_csv('0204Bin.csv')
 
-data=data1.loc[data1['not_PEC/QTZ_SOURCE_APPLICATION_ID_MI'] == 1]
 
-data=data.loc[:, (data != 0).any(axis=0)]#deleting columns with all values in the column=0
+del data1['QTZ_SOURCE_APPLICATION_ID_MI']
+del data1['PEC_SOURCE_APPLICATION_ID_MI']
+del data1['BOTH_PEC&QTZ_SOURCE_APPLICATION_ID_MI']
+
+
+#del data1['NOT_PEC_AND_QTZ_SOURCE_APPN_IDS_MI']
+del data1['PEC_SOURCE_APPN_IDS_MI']
+del data1['QTZ_SOURCE_APPN_IDS_MI']
+del data1['QTZ_and_TAV_SOURCE_APPN_IDS_MI']
+del data1['QTZ_and_TOP_SOURCE_APPN_IDS_MI']
+del data1['MPA_and_QTZ_SOURCE_APPN_IDS_MI']
+del data1['PEC_and_QTZ_SOURCE_APPN_IDS_MI']
+data=data1.loc[data1['NOT_PEC_AND_QTZ_SOURCE_APPN_IDS_MI'] == 1]
+
 gg=data.copy()
-del data[ 'PEC_SOURCE_APPLICATION_ID_MI']
-del data['not_PEC/QTZ_SOURCE_APPLICATION_ID_MI']
-
+del data['NOT_PEC&QTZ_SOURCE_APPLICATION_ID_MI']
+data=data.loc[:, (data != 0).any(axis=0)]#deleting columns with all values in the column=0
 decisionTree = growDecisionTreeFrom(data,cnsqnt,ante,gg)
-#result = plot(decisionTree)
-'''dot_data = dotgraph(decisionTree)
-graph = pydotplus.graph_from_dot_data(dot_data)
-graph.write_png("iris.png")'''
+
+cons = data1['SECONDBIT=0_APP_ORGIN_RCG']
+l = len(cons)
+for i in range(len(gr)):
+    
+    ant = [1 for i in range(l)]
+    tp=0
+    fn=0
+    for j in gr[i]:
+        ant=[ant[i1]*data1[j][i1] for i1 in range(l)]
+    for k in range(l):
+        if(ant[k]==1 and cons[k] == 1):
+            tp+=1
+        elif(ant[k] == 1 and cons[k] == 0):
+            fn+=1
+            
+    gr[i].append([tp, fn])
+    
+edge=[]
+for i in gr:
+    if len(i)<3:
+        edge.append((i[-2], i[-1]))
+    else:
+        edge.append((i[-3], i[-2], i[-1]))
+        
+nodes=[]
+t=edge[0][1][0]
+f = edge[0][1][1]
+
+s = round(t/(t+f),3)
+nodes.append(edge[0][0]+"\nTp"+str(t)+"\Fn"+str(f)+"\nS="+str(s))
+del edge[0]
+main_edges=[]
+
+for i, j, m in edge:
+    t=m[0]
+    f=m[1]
+    s = round(t/(t+f), 3)
+    j=j+"\nTp="+str(t)+"\nFn="+str(f)+"\nS="+str(s)
+    nodes.insert(0, j)
+    for k in nodes:
+        if i in k:
+            i=k
+            break
+    if((i, j) not in main_edges):
+        main_edges.append((i, j))
+        
+import graphviz
+
+G = graphviz.Digraph(node_attr={'shape':'box', 'fontsize':'12', 'fontname':'verdana'}, format='png')
+for i, j in main_edges:
+    G.edge(i, j)
+    
+G.view()
